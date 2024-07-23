d022e8f8a99399ee7038dc09c4e72c9720402300
akechsalim <akechsalimwork@gmail.com>
2024-07-23 21:43:07 +0300

    update fronted module added

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
-import { createEmployee } from '../services/EmployeeService'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { createEmployee, getEmployee, updateEmployee } from '../services/EmployeeService'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const EmployeeComponent = () => {
 
@@ -10,18 +10,94 @@ const EmployeeComponent = () => {
 
     const [email, setEmail] = useState('')
 
+    const [errors, setError] = useState({
+        firstName: '',
+        lastName: '',
+        email: ''
+    })
+    const { id } = useParams();
+
+    useEffect(() => {
+        if (id) {
+            getEmployee(id).then((response) => {
+                setFirstName(response.data.firstName);
+                setLastName(response.data.lastName);
+                setEmail(response.data.email);
+
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+    }, [])
+
+
     const navigator = useNavigate();
 
-    function saveEmployee(e) {
+    function saveOrUpdateEmployee(e) {
 
         e.preventDefault();
-        const employee = { firstName, lastName, email }
-        console.log(employee)
 
-        createEmployee(employee).then((response) => {
-            console.log(response.data)
-            navigator('/employees')
-        })
+        if (validateForm()) {
+
+            const employee = { firstName, lastName, email }
+            console.log(employee)
+
+            if (id) {
+                updateEmployee(id, employee).then((response) => {
+                    console.log(response.data);
+                    navigator('/employees')
+                }).catch(error => {
+                    console.log(error);
+                })
+            } else {
+                createEmployee(employee).then((response) => {
+                    console.log(response.data)
+                    navigator('/employees')
+                }).catch(error => {
+                    console.log(error);
+
+                })
+            }
+        }
+    }
+
+
+    function validateForm() {
+        let valid = true;
+        const errorsCopy = { ...errors }
+
+        if (firstName.trim()) {
+            errorsCopy.firstName = '';
+        } else {
+            errorsCopy.firstName = 'First name is required';
+            valid = false;
+        }
+
+        if (lastName.trim()) {
+            errorsCopy.lastName = '';
+        } else {
+            errorsCopy.lastName = 'Last name is required';
+            valid = false;
+        }
+        if (email.trim()) {
+            errorsCopy.email = '';
+        } else {
+            errorsCopy.email = 'email is required';
+            valid = false;
+        }
+
+        setError(errorsCopy);
+
+        return valid;
+
+    }
+    function pageTitle() {
+        if (id) {
+            return <h2 className='text-center'>Update Employee</h2>
+        } else {
+            return <h2 className='text-center'>Add Employee</h2>
+        }
+
     }
 
     return (
@@ -29,7 +105,9 @@ const EmployeeComponent = () => {
             <br></br>
             <div className='row'>
                 <div className='card col-md-6 offset-md-3 offset-md-3'>
-                    <h2 className='text-center'>Add Employee</h2>
+                    {
+                        pageTitle()
+                    }
                     <div className='card-body'>
                         <form>
                             <div className='form-group md-2'>
@@ -39,10 +117,11 @@ const EmployeeComponent = () => {
                                     placeholder='Enter First Name'
                                     name='firstName'
                                     value={firstName}
-                                    className='form-control'
+                                    className={`form-control ${errors.firstName ? 'is-invalid' : ''}`}
                                     onChange={(e) => setFirstName(e.target.value)}
                                 >
                                 </input>
+                                {errors.firstName && <div className='invalid-feedback'>{errors.firstName}</div>}
 
                             </div>
 
@@ -53,10 +132,11 @@ const EmployeeComponent = () => {
                                     placeholder='Enter Last Name'
                                     name='lastName'
                                     value={lastName}
-                                    className='form-control'
+                                    className={`form-control ${errors.lastName ? 'is-invalid' : ''}`}
                                     onChange={(e) => setLastName(e.target.value)}
                                 >
                                 </input>
+                                {errors.lastName && <div className='invalid-feedback'>{errors.lastName}</div>}
 
                             </div>
 
@@ -67,14 +147,15 @@ const EmployeeComponent = () => {
                                     placeholder='Enter Email'
                                     name='email'
                                     value={email}
-                                    className='form-control'
+                                    className={`form-control ${errors.email ? 'is-invalid' : ''}`}
                                     onChange={(e) => setEmail(e.target.value)}
                                 >
                                 </input>
+                                {errors.email && <div className='invalid-feedback'>{errors.lastName}</div>}
 
                             </div>
 
-                            <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
+                            <button className='btn btn-success' onClick={saveOrUpdateEmployee}>Submit</button>
                         </form>
 
                     </div>
--- ems-frontend/src/components/ListEmployeeComponent.jsx
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -17,17 +17,51 @@ const ListEmployeeComponent = () => {
         })
     }, [])
 
-    function addNewEmployee(){
+    function addNewEmployee() {
         navigator('/add-employee')
 
+    }
+
+    function updateEmployee(id) {
+        navigator(`/edit-employee/${id}`)
     }
 
 
     return (
         <div className='container'>
-            <button className='btn btn-primary mb-2' onClick={addNewEmployee}>Add Employee</button>
+            <h2 className='text-center'>List of employees</h2>
+            <table className='table table-striped table-bordered'>
+                <thead>
+                    <tr>
+                        <th>EmployeeId</th>
+                        <th>Employee First Name</th>
+                        <th>Employee Last Name</th>
+                        <th>Employee Email</th>
+                        <th>Actions</th>
+                    </tr>
 
+                </thead>
+                <tbody>
+                    {
+                        employees.map(employee =>
+                            <tr key={employee.id}>
+                                <td>{employee.id}</td>
+                                <td>{employee.firstName}</td>
+                                <td>{employee.lastName}</td>
+                                <td>{employee.email}</td>
+                                <td>
+                                    <button className='btn btn-info' onClick={() => updateEmployee(employee.id)}>update</button>
+                                </td>
+                            </tr>)
+                    }
+
+
+                </tbody>
+            </table>
+
+            <button className='btn btn-primary mb-2' onClick={addNewEmployee}>Add Employee</button>
         </div>
+
     )
 }
 
